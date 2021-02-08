955dfb20578ba903c258e2d3d36e40b3bb9563b7
florent Richard <florent.r08@live.fr>
2021-02-08 12:10:56 +0100

    Ajout des déplacements, des combats et de la mort

--- src/controleur/Global.java
+++ src/controleur/Global.java
@@ -143,6 +143,14 @@ public interface Global {
 	 */
 	int HAUTEURMESSAGE = 8;
 	/**
+	 * largeur de la boule
+	 */
+	int LARGEURBOULE = 17;
+	/**
+	 * hauteur de la boule
+	 */
+	int HAUTEURBOULE = 17;
+	/**
 	 * orientation du personnage vers la gauche
 	 */
 	int GAUCHE = 0;
@@ -150,6 +158,22 @@ public interface Global {
 	 * orientation du personnage vers la droite
 	 */
 	int DROITE = 1;
+	/**
+	 * taille du pas quand le personnage avance (nombre de pixels)
+	 */
+	int PAS = 10;
+	/**
+	 * nombre d'étapes (d'images) pour donner l'impresson de marche
+	 */
+	int NBETAPESMARCHE = 4;
+	/**
+	 * nombre d'étapes (d'images) pour donner l'impression d'être touché
+	 */
+	int NBETAPESTOUCHE = 2;
+	/**
+	 * nombre d'étapes (d'images) pour donner l'impresson de mourir
+	 */
+	int NBETAPESMORT = 2;
 	/**
 	 * Message "serveur" pour la création d'un serveur
 	 */
@@ -186,5 +210,9 @@ public interface Global {
 	 * ordre pour modifier le contenu du tchat dans l'arène du client
 	 */
 	String MODIFTCHAT = "modif tchat";
+	/**
+	 * ordre pour exécuter une action (déplacement, tire de boule)
+	 */
+	String ACTION = "action";
 
 }
